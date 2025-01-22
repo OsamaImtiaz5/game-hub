3b444f51a481aea1e3251f4f092009bb91446cb1
OsamaImtiaz5 <bsse07191009@student.uol.edu.pk>
2025-01-22 18:26:35 +0500

    ceate custom hook to fetch games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,37 +1,8 @@
-import React, { useState, useEffect } from "react";
-import apiClient from "./services/api-client";
 import { Text } from "@chakra-ui/react";
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchGamesProps {
-  count: number;
-  results: Game[];
-}
+import { useGames } from "./hooks/useGames";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    apiClient
-      .get<FetchGamesProps>("/games")
-      .then((res) => {
-        console.log("API Response:", res.data); // Log the response data
-        if (res.data && res.data.results) {
-          setGames(res.data.results);
-        } else {
-          console.warn("No data in response");
-        }
-      })
-      .catch((err) => {
-        console.error("API Error:", err); // Log the error
-        setError(err.message);
-      });
-  }, []);
+  const { games, error } = useGames();
 
   return (
     <>
---
+++ src/components/hooks/useGames.ts
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+
+interface Game {
+  id: number;
+  name: string;
+}
+
+interface FetchGamesProps {
+  count: number;
+  results: Game[];
+}
+export const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState<string | null>(null);
+  useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+    apiClient
+      .get<FetchGamesProps>("/xgames", { signal })
+      .then((res) => {
+        console.log("API Response:", res.data); // Log the response data
+        if (res.data && res.data.results) {
+          setGames(res.data.results);
+        } else {
+          console.warn("No data in response");
+        }
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+
+        console.error("API Error:", err); // Log the error
+        setError(err.message);
+      });
+    return () => {
+      controller.abort();
+    };
+  }, []);
+  return { games, error };
+};
