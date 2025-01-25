61ae6864a61925110d4bc81be34fd588a99da653
OsamaImtiaz5 <bsse07191009@student.uol.edu.pk>
2025-01-26 02:14:14 +0500

    Showing spinner while fecthing genres

--- src/components/GenersList.tsx
+++ src/components/GenersList.tsx
@@ -1,14 +1,13 @@
-import { HStack, Image, List, Text } from "@chakra-ui/react";
+import { HStack, Image, List, Spinner, Text } from "@chakra-ui/react";
 import useGeners from "./hooks/useGeners";
 import getCroppedImageUrl from "./services/image-url";
 
 const GenersList = () => {
-  const { data, error } = useGeners();
-
+  const { data, error, loading } = useGeners();
+  if (loading) return <Spinner />;
+  if (error) return null;
   return (
     <div>
-      {error && <Text>{error}</Text>}
-
       {data.map((genre) => (
         <List.Root>
           <List.Item key={genre.id} listStyleType="none" paddingY="5px">
@@ -19,7 +18,7 @@ const GenersList = () => {
                 boxSize="32px"
                 borderRadius={8}
               />
-              <Text fontSize='lg'> {genre.name}</Text>
+              <Text fontSize="lg"> {genre.name}</Text>
             </HStack>
           </List.Item>
         </List.Root>
